5cd4db37c90131e4393b870c83e57658329a45a8
usuwo <hikaru.usuwo@gmail.com>
2021-02-20 15:21:19 +0900

    一旦休憩

---
+++ main/gyogyo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http      import HttpResponse
+import calendar
+
+# Create your views here.
+def index(request):
+    lc = calendar.HTMLCalendar()
+    body = lc.formatyear(2020, width=4)
+    return HttpResponse(body)
